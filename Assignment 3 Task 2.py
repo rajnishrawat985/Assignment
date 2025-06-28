4210ae50e9d82b25d748d4533de59a002262fdcd
rajnishrawat985 <109939015+rajnishrawat985@users.noreply.github.com>
2025-06-28 21:23:24 +0530

    Add files via upload

---
+++ Assignment 3 Task 2.py
@@ -0,0 +1,8 @@
+import math
+num = float(input("Enter a number: "))
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+print(f"square root: ",square_root)
+print(f"natural log: ",natural_log)
+print(f"sine value: ",sine_value)
